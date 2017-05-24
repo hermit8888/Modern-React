6cde6ec1a4821c1c5f1c792896807455ae2252bb
hermit8888 <ahawes777@gmail.com>
2017-05-24 11:51:26 -0700

    youtube_clone_final

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video, onVideoSelect}) => {
+  // putting {video} as a function property above
+  // is the same as: const video = props.video;
+  // in the block.
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list-media">
+        <img className="media-object" src={imageUrl} alt="" />
+      </div>
+      <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
